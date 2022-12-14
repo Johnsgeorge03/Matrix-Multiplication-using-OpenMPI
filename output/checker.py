9376c60348adbbe5633b8b13b169c557d11fd113
John George <john@Johns-MacBook-Pro.local>
2022-12-13 20:55:28 -0600

    Modified print statements to give more info

--- output/checker.py
+++ output/checker.py
@@ -23,12 +23,14 @@ with open('/Users/john/Programs/parallel_matmul/output/mat.txt', 'r') as f:
 A = np.array(a)
 B = np.array(b)
 C = np.array(c)
-print(A.shape)
-print(B.shape)
-print(C.shape)
+print("Matrix Shapes")
+print("A ", A.shape)
+print("B ", B.shape)
+print("C ", C.shape)
 Ccomp = np.around(A@B, 10)
 #print(C - Ccomp)
-print(np.linalg.norm(C - Ccomp, ord='fro'))
+print("Frobenius norm: ", np.linalg.norm(C - Ccomp, ord='fro'))#/np.sqrt(C.shape[0]*C.shape[1]))
+print("\n")
 
        
 
